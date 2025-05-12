2c5ca09468656c08ab74335d44adafa9075919ae
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-05-12 17:33:29 +0200

    Feat(Navigation): Add Routing
    Add Routing just for the newsletter

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
+import Newsletter from '../Page/Newsletter';
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Newsletter/>}/>
+      </Routes>
     </div>
   );
 }
